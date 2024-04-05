2d155f88afa9d6a7f42130f72f39e6fa35117242
Matthew Zhang <gzhang@lcs.on.ca>
2024-04-05 09:54:00 -0400

    Initial Commit

---
+++ To-do List App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-do List App
+//
+//  Created by GengYu Zhang on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-do List App/To_do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_List_AppApp.swift
+//  To-do List App
+//
+//  Created by GengYu Zhang on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct To_do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
